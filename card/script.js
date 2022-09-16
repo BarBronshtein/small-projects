3bdfb07fbc2963c7b49154228e6ac39b7bdcac77
BarBronshtein <barbronshtein@gmail.com>
2022-09-16 21:15:05 +0300

    added selecting sizes

--- card/script.js
+++ card/script.js
@@ -1,10 +1,21 @@
 const container = document.querySelector('.container');
 const color1 = document.querySelector('.color-1');
 const color2 = document.querySelector('.color-2');
-
+const sizes = document.querySelectorAll('.sizes span');
+console.log(sizes);
 color2.addEventListener('click', () => {
 	container.classList.add('change');
 });
 color1.addEventListener('click', () => {
 	container.classList.remove('change');
 });
+
+sizes.forEach(el =>
+	el.addEventListener('click', () => {
+		const oldActive = document
+			.querySelector('.size span.active')
+			.classList.remove('active');
+
+		el.classList.add('active');
+	})
+);
